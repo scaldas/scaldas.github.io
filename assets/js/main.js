564dd60eab90c55494d2f115c02911f24ff344e7
Sebastian Caldas <sebastian.caldas@gmail.com>
2018-01-28 21:32:31 -0500

    better menu bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,6 +102,7 @@
 				);
 			});
 
+		/**
 		// Menu.
 			var $menu = $('#menu');
 
@@ -194,7 +195,28 @@
 							$menu._hide();
 
 				});
-
+			*/
 	});
+
+	$(function(){
+    $(window).scroll(function(){
+        if($(window).scrollTop() >= 1){ //if user has scrolled the window
+            $('.backToTop').fadeIn(500); // show the back to top button
+        }else { // else if user is at the top already
+            $('.backToTop').fadeOut(500); //hide the back to top button
+        }
+    });
+
+    //scroll the page to the top if user clicks on the back to top button
+    $('.backToTop').click(function(){
+        $('html,body').animate({ scrollTop: 0 }, 'slow');
+    });
+
+    //checks if page was already scrolled down before load
+    if($(window).scrollTop() >= 1){
+        $('.backToTop').show(); // show the back to top button
+    }
+});
+
 
 })(jQuery);
